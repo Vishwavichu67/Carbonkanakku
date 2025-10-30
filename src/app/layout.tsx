9aa8e1f839528d640ecce47ce2261b707e4022ed
ᴠ ɪ s ʜ ᴡ ᴀ <vishwaceo67@gmail.com>
2025-10-30 17:04:23 +0000

    also use the favicon.ico as favicon for this site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+  }
 };
 
 export default function RootLayout({
