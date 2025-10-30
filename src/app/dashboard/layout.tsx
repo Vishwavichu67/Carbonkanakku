3b7e325bf6c67c570ac002c930224150406d4dee
ᴠ ɪ s ʜ ᴡ ᴀ <vishwaceo67@gmail.com>
2025-10-30 14:29:16 +0000

    remove this (_for element <LinkComponent>_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,16 +2,12 @@ import Link from "next/link";
 import {
   SidebarProvider,
   Sidebar,
-  SidebarHeader,
   SidebarContent,
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
-  SidebarFooter,
 } from "@/components/ui/sidebar";
-import { Logo } from "@/components/logo";
 import { dashboardNav } from "@/lib/constants";
-import { LogOut, Settings } from "lucide-react";
 import { SiteHeader } from "@/components/site-header";
 
 export default function DashboardLayout({
@@ -46,18 +42,6 @@ export default function DashboardLayout({
                 ))}
               </SidebarMenu>
             </SidebarContent>
-            <SidebarFooter>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                    <SidebarMenuButton asChild tooltip={{children: 'Log Out'}}>
-                        <Link href="/">
-                            <LogOut />
-                            <span>Log Out</span>
-                        </Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarFooter>
           </Sidebar>
           <main className="flex-1 bg-secondary/50">
             <div className="container py-8">{children}</div>
